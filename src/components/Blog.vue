725673e81702b0c1ae22338bb0187130c2794466
Délita Makanda <delita.makanda@gmail.com>
2018-03-04 17:08:09 +0100

    change signup component to blog

---
+++ src/components/Blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ subtitle }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+  data () {
+    return {
+      subtitle: 'Blog'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
