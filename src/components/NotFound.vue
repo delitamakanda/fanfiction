5439b817f60e7520651b04b95bd66b823db6ce1c
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-03-05 13:01:21 +0100

    Create NotFound.vue

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>This page doesn't exist</h1>
+        <!-- TODO:  -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+    data(){
+        return{
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
