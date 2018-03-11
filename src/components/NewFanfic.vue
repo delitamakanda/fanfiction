588aa256c72d767d9ed4c6905c58cbbcaa5cb0ef
Délita Makanda <delita.makanda@gmail.com>
2018-03-12 00:33:05 +0100

    cascading menu

--- src/components/NewFanfic.vue
+++ src/components/NewFanfic.vue
@@ -14,7 +14,7 @@
               <div class="relative">
                   <select class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="category" name="category" v-if="loadingCategory" v-model="category">
                       <option value="">Sélectionner</option>
-                      <option v-for="(option, index) in dataCategories" :value="option.id">{{ option.name }}</option>
+                      <option v-for="(option, index) in dataCategories" v-bind:value="option.id">{{ option.name }}</option>
                   </select>
                   <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
                       <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -26,9 +26,9 @@
                   Sous - Catégories
               </label>
               <div class="relative">
-                  <select class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="subcategory" name="subcategory" v-if="loadingSubCategory" v-model="subcategory">
+                  <select :disabled="category.length == 0" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="subcategory" name="subcategory" v-if="loadingSubCategory" v-model="subcategory">
                       <option value="">Sélectionner</option>
-                      <option v-for="(option, index) in dataSubCategories" :value="option.id">{{ option.name }}</option>
+                      <option v-for="(option, index) in dataSubCategories" v-if="option.category === category" v-bind:value="option.id">{{ option.name }}</option>
                   </select>
                   <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
                       <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -155,6 +155,7 @@ export default {
             dataGenresFormatted: [],
             dataCategories: [],
             dataSubCategories: [],
+            resCat: null,
             title: '',
             description: '',
             synopsis: '',
@@ -227,14 +228,18 @@ export default {
         async getCategories () {
             this.dataCategories = await this.$fetch('category')
             this.loadingCategory = true
-            console.log(this.dataCategories)
         },
 
         async getSubCategories () {
             this.dataSubCategories = await this.$fetch('subcategory')
             this.loadingSubCategory = true
-            console.log(this.dataSubCategories)
         },
+    },
+    watch: {
+        category () {
+
+            if (this.dataCategories.length > 0) {}
+        }
     },
     created () {
         this.getGenres()
