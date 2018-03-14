f054da7dc8aff5a4866fcb272c474d1754a5ece7
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-03-14 13:27:04 +0100

    forgot classnames for transition

--- src/App.vue
+++ src/App.vue
@@ -37,4 +37,12 @@ export default {
   color: #2c3e50;
   margin-top: 15px;
 }
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
