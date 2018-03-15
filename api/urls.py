40cc4a8a218ed1ff58cc8e1f4ac09614816cf066
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-03-15 17:15:36 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from api import views
+from api import api
 
 urlpatterns = [
     path('fanfics', views.FanficList.as_view(), name='fanfic-list'),
@@ -20,9 +21,9 @@ urlpatterns = [
     path('comments/<int:pk>', views.CommentDetail.as_view(), name='comment-detail'),
     path('users', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
-    path('user', views.CheckoutUserView.as_view(), name='user'),
-    path('signup', views.UserCreate.as_view(), name='signup'),
-    path('login', views.LoginView.as_view(), name='login'),
-    path('logout', views.LogoutView.as_view(), name='logout'),
+    path('user', api.CheckoutUserView.as_view(), name='user'),
+    path('signup', api.UserCreate.as_view(), name='signup'),
+    path('login', api.LoginView.as_view(), name='login'),
+    path('logout', api.LogoutView.as_view(), name='logout'),
     path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
