ffeaab27ab8135560dae3dd43712cd8046ed3533
Délita Makanda <delita.makanda@gmail.com>
2018-03-15 20:41:09 +0100

    change transition

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@ export default {
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+  transition: opacity .1s;
 }
 
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
