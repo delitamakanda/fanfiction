6785644341813d2133efd99af0060fa9444e9029
Délita Makanda <delita.makanda@gmail.com>
2018-03-17 12:56:59 +0100

    add backend email

--- backend/settings.py
+++ backend/settings.py
@@ -176,3 +176,10 @@ REST_FRAMEWORK = {
     'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata',
     # 'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
