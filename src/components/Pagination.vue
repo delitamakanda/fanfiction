56acc9c7781e933c51f6343939bd0520f7b3f74b
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-03-27 16:38:25 +0200

    Create Pagination.vue

---
+++ src/components/Pagination.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.pagination {
+  list-style-type: none;
+}
+
+.pagination-item {
+  display: inline-block;
+}
+
+.active {
+  background-color: #4aae98;
+  color: #fff;
+}
+</style>
