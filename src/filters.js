a6fde934c5535a0588844c968750a6179d16e223
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-04-03 11:08:07 +0200

    Update filters.js

--- src/filters.js
+++ src/filters.js
@@ -3,3 +3,11 @@ import moment from 'moment'
 export function date (value) {
   return moment(value).format('DD/MM/YYYY')
 }
+
+export function uppercase (name) {
+  if (typeof name !== "string") {
+    throw TypeError("name must be a string")
+  }
+  
+  return name.toUpperCase()
+}
