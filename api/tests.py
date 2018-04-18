c6985df7159ab85772c92c7bc2284872db06b8db
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-04-18 11:28:03 +0200

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.six import BytesIO
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.serializers import CommentSerializer
 from api.models import Fanfic
+from api.models import Comment
 from api import views
+
 # Create your tests here.
 
 class FanficTests(APITestCase):
@@ -14,13 +20,20 @@ class FanficTests(APITestCase):
         response = self.client.post(url, data, format='json')
         return response
 
-    # def test_post_and_get_fanfic(self):
-    #     """
-    #     Ensure we can create a new Fanfic and then retrieve it
-    #     """
-    #     new_fanfic = 'A Story'
-    #     response = self.post_fanfic(new_fanfic)
-    #     print("ID {0}".format(Fanfic.objects.get().id))
-    #     assert response.status_code == status.HTTP_201_CREATED
-    #     assert Fanfic.objects.count() == 1
-    #     assert Fanfic.objects.get().title == new_fanfic
+    def test_post_and_get_fanfic(self):
+         """
+         Ensure we can create a new Fanfic and then retrieve it
+         """
+         new_fanfic = 'A Story'
+         response = self.post_fanfic(new_fanfic)
+         print("ID {0}".format(Fanfic.objects.get().id))
+         assert response.status_code == status.HTTP_201_CREATED
+         assert Fanfic.objects.count() == 1
+         assert Fanfic.objects.get().title == new_fanfic
+      
+
+class CommentSerializerTests(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+    cls.c = Comment(author="test", email="test@test.com", body="test body")
+    cls.c.save()
