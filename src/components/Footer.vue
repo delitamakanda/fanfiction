ba42c53343956b98bdd916672d782d41c074b23d
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-05-29 15:15:06 +0200

    add modal

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,46 @@
 <template>
     <footer>
-        <p>Copyright 2018 {{ title }}</p>
+        <p>Copyright 2018 {{ title }} - <a href="#">Politique de confidentialité</a> - <a @click="showModal">Mentions légales</a></p>
     </footer>
+    
+    <modal
+      v-show="isModalVisible"
+      @close="closeModal"
+    >
+    <h3 slot="header">Mentions légales</h3>
+    <div slot="body">
+        // TODO: 
+    </div>
+    </modal>
 </template>
 
 <script>
+import modal from './Modal.vue';
+
 export default {
     props: {
       title: {
         type: String,
-        required: true
+        required: true,
       }
     },
+    components: {
+     modal,
+   },
     data(){
         return{
+          isModalVisible: false,
         }
-    }
+    },
+    methods: {
+      showModal () {
+         this.isModalVisible = true;
+      },
+      
+      closeModal() {
+         this.isModalVisible = false;
+       },
+    },
 }
 </script>
 
