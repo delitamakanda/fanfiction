fc3779615a50b2ec460d6f7a0016821baf9dd74d
Délita Makanda <delita.makanda@gmail.com>
2018-06-17 16:44:54 +0200

    add prod files

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'multiselectfield',
     'embed_video',
     'django_filters',
+    'storages',
     #'allauth',
     #'allauth.account',
 ]
--- backend/settings_production.py
+++ backend/settings_production.py
@@ -1,1 +1,32 @@
+from backend.settings import *
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+DEBUG = config('DEBUG', cast=bool)
+
+# email admin
+
+SERVER_EMAIL = config('ADMIN_EMAIL')
+
+ADMINS = [
+  (config('ADMIN_NAME'), config('ADMIN_EMAIL')),
+]
+
+ALLOWED_HOSTS = ['*',]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Media storages
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+DEFAULT_FILE_STORAGE = 'backend.storage_backends.MediaStorage'
