bee33c1cd671b36725db9a3567a55923146e6082
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-06-24 23:11:22 +0200

    Update settings_production.py

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -43,7 +43,8 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+# EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('SMS_HOST')
 EMAIL_PORT = config('SMS_PORT')
 EMAIL_HOST_USER = config('SMS_USERNAME')
