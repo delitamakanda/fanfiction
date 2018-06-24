542e90217ede7d4f68b5117cd8a6481c4badad90
Délita Makanda <delita.makanda@gmail.com>
2018-06-24 23:03:22 +0200

    add email sample

--- backend/settings.py
+++ backend/settings.py
@@ -191,8 +191,3 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = ''
-EMAIL_PORT = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
--- backend/settings_production.py
+++ backend/settings_production.py
@@ -42,3 +42,10 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
+
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_HOST = config('SMS_HOST')
+EMAIL_PORT = config('SMS_PORT')
+EMAIL_HOST_USER = config('SMS_USERNAME')
+EMAIL_HOST_PASSWORD = config('SMS_PASSWORD')
+EMAIL_USE_TLS = config('SMS_TLS')
