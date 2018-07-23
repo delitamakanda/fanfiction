c0060e2036478d84f3dacecf45f819e70cd5b633
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-07-23 23:54:47 +0200

    Update settings_production.py

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -47,30 +47,13 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-        'TIMEOUT': None,
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'TIMEOUT': 500,
+        'BINARY': True,
         'LOCATION': config('MEMCACHIER_SERVERS'),
         'OPTIONS': {
-            'binary': True,
             'username': config('MEMCACHIER_USERNAME'),
-            'password': config('MEMCACHIER_PASSWORD'),
-            'behaviors': {
-                # Enable faster IO
-                'no_block': True,
-                'tcp_nodelay': True,
-                # Keep connection alive
-                'tcp_keepalive': True,
-                # Timeout settings
-                'connect_timeout': 2000, # ms
-                'send_timeout': 750 * 1000, # us
-                'receive_timeout': 750 * 1000, # us
-                '_poll_timeout': 2000, # ms
-                # Better failover
-                'ketama': True,
-                'remove_failed': 1,
-                'retry_timeout': 2,
-                'dead_timeout': 30,
-            }
+            'password': config('MEMCACHIER_PASSWORD')
         }
     }
 }
