6542e103e1e59648befbcae6b33993111199e268
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-06 15:13:32 +0200

    Update email_reminder.py

--- api/management/commands/email_reminder.py
+++ api/management/commands/email_reminder.py
@@ -5,7 +5,6 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 from django.template.loader import get_template 
-from django.template import Context
 
 def email_tardy_users():
     two_weeks_ago = now() - timedelta(days=14)
@@ -15,9 +14,9 @@ def email_tardy_users():
 
     for user in tardy_users:
         template = get_template('mail/login_reminder.txt') 
-        context = Context({
+        context = {
             'username': user.username,
-        })
+        }
 
         content = template.render(context)
         send_mail(
