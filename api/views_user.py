a12ccd13e04780ba4fb4d019db0aec12ba241ac7
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-09 11:34:10 +0200

    Update views_user.py

--- api/views_user.py
+++ api/views_user.py
@@ -1,1 +1,23 @@
+import json
 
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions, views, status, viewsets
+
+from api.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    """
+    List all users
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    Retrieve an user
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
