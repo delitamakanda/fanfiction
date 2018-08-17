ce2131fcfed647f899f91608f8e892f82dabf99a
Délita Makanda <delita.makanda@gmail.com>
2018-08-17 22:05:08 +0200

    refactor folder help

--- api/models.py
+++ api/models.py
@@ -222,3 +222,17 @@ class FlatPages(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+"""
+Models for html pages
+"""
+
+class Lexique(models.Model):
+    title = models.CharField(max_length=30, db_index=True)
+    definition = models.TextField(max_length=500)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
