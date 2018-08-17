bc3d92ba753d7f072c906a961c11a2c88bdf81c1
Délita Makanda <delita.makanda@gmail.com>
2018-08-18 00:03:36 +0200

    add paths

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,7 +32,17 @@
                 </template>
                 <router-link v-else :to="{ name: 'Login' }" class="block mt-4 lg:inline-block lg:mt-0 text-teal hover:text-teal-darker mr-4">Se connecter</router-link>
                 <router-link :to="{ name: 'Posts' }" class="block mt-4 lg:inline-block lg:mt-0 text-teal hover:text-teal-darker mr-4">News</router-link>
-                <a class="block mt-4 lg:inline-block lg:mt-0 text-teal hover:text-teal-darker" href="/help/browse/title"><svgicon icon="question" width="22" height="18" color="#4dc0b5"></svgicon> Aide</a>
+                <ul class="menu list-reset block mt-4 lg:inline-block lg:mt-0 text-teal hover:text-teal-darker mr-4">
+                    <li @mouseover="survol = true" @mouseleave="survol = false"><svgicon icon="question" width="22" height="18" color="#4dc0b5"></svgicon> Aide
+                        <transition name="fade" mode="out-in">
+                            <ul class="bg-white p-4 shadow " v-if="survol" @click="survol = false">
+                                <li><a class="text-teal hover:text-teal-darker" href="/help/browse/title">Lexique</a></li>
+                                <li><a class="text-teal hover:text-teal-darker" href="#">Foire aux questions (A venir)</a></li>
+                                <li><a class="text-teal hover:text-teal-darker" href="#">Contacter le webmestre (A venir)</a></li>
+                            </ul>
+                        </transition>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
@@ -52,6 +62,7 @@ export default {
     data(){
         return{
             open: false,
+            survol: false,
         }
     },
     methods: {
@@ -74,5 +85,16 @@ export default {
 <style scoped>
 nav {
     margin-bottom: 14px;
+}
+.menu li {
+    position: relative;
+    min-width: 180px;
+}
+.menu li ul {
+    position: absolute;
+    left: 0;
+    top: 1.5rem;
+    list-style-type: disc;
+    list-style-position: inside;
 }
 </style>
