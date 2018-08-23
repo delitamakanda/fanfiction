f9322da90d23d9d6620b3941819cb1851e18c8a1
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-24 01:09:57 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'dist'),
+    os.path.join(BASE_DIR, 'dist'),
     os.path.join(BASE_DIR, 'static'),
 )
 
