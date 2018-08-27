ac58d2a6df2e0dabe0009d69aab1e7e72ff46ea2
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-27 15:52:38 +0200

    Update urls.py

--- api/helpcenter/urls.py
+++ api/helpcenter/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('browse/title/<str:initial>', views.browse_by_title, name='browse_by_title'),
     path('search', views.SearchSubmitView.as_view(), name='search'),
     path('search-ajax-submit', views.SearchAjaxSubmitView.as_view(), name='search-ajax-submit'),
+    path('faq', views.foire_aux_questions_view, name='foire_aux_questions'),
 ]
