3ef6431e7bb3473b4aab730bdff527f3245c710b
Délita Makanda <delita.makanda@gmail.com>
2018-08-28 21:31:04 +0200

    styles: news

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,54 +1,55 @@
 <template>
-  <div>
-
-    <div class="error bg-red-lightest border border-red-light text-red-dark px-4 py-3 rounded relative" v-if="hasRemoteErrors" role="alert">
-        {{ errorFetch }}
-    </div>
-
-    <section>
+    <div>
         <Loading v-if="remoteDataBusy" />
-
-        <div class="overflow-hidden" v-for="item in postList" track-by="item.id">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">
-                    <router-link :to="{
-                        name: 'PostDetail',
-                        params: {
-                          slug: item.slug
-                        },
-                    }" class="block mt-4 lg:inline-block lg:mt-0 text-teal hover:text-teal-darker">
-                        <h3>{{ item.title }}</h3>
-                    </router-link>
-                </div>
-                <p class="text-grey-darker text-base"> Par {{ item.user }}</p>
-                <p class="text-grey-darker text-base"> Publiée le {{ item.created | date }}</p>
-            </div>
-            <div class="px-6 py-4">
-                <span v-for="tag in item.tags" class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
-                    {{ tag.word }}
-                </span>
-            </div>
+        <div class="error bg-red-lightest border border-red-light text-red-dark px-4 py-3 rounded relative" v-if="hasRemoteErrors" role="alert">
+            {{ errorFetch }}
         </div>
-    </section>
 
-  </div>
+        <section>
+            <div class="max-w-md w-full lg:flex mb-4" v-for="item in postList" track-by="item.id">
+                <div v-if="item.header" class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" :style="{backgroundImage: 'url(' + item.header + ')' }" :title="item.title">
+                </div>
+                <div v-else class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" :style="{backgroundColor: '#ddd' }" :title="item.title"></div>
+                <router-link :to="{name: 'PostDetail', params: {slug: item.slug},}" class="no-underline">
+                    <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                        <div class="mb-8">
+                            <p class="text-sm text-grey-dark flex items-center">
+                                <span v-for="tag in item.tags" class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
+                                    #{{ tag.word }}
+                                </span>
+                            </p>
+                            <div class="text-black font-bold text-xl mb-2">{{ item.title }}</div>
+                            <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                        </div>
+                        <div class="flex items-center">
+                            <!--<img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">-->
+                            <div class="text-sm">
+                                <p class="text-black leading-none">Par {{ item.user }}</p>
+                                <p class="text-grey-dark">Publiée le {{ item.created | date }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
 import RemoteData from '../mixins/RemoteData'
 
 export default {
-  name: 'Posts',
-  mixins: [
-      RemoteData({
-          postList: 'posts',
-      }),
-  ],
-  data () {
-    return {
-      errorFetch: 'Il y a un problème avec la requète.'
+    name: 'Posts',
+    mixins: [
+        RemoteData({
+            postList: 'posts',
+        }),
+    ],
+    data () {
+        return {
+            errorFetch: 'Il y a un problème avec la requète.'
+        }
     }
-}
 }
 </script>
 
