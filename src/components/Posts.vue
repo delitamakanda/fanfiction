2bb4f344fa08f74cef5ebe2d8fcfe1bd8a4a981d
Délita Makanda <delita.makanda@gmail.com>
2018-08-30 22:27:21 +0200

    fix components post

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -19,7 +19,7 @@
                                 </span>
                             </p>
                             <div class="text-black font-bold text-xl mb-2">{{ item.title }}</div>
-                            <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                            <vue-markdown :source="item.content" class="text-grey-darker text-base">{{ item.content.length > 25 ? item.content.substring(0, 25) + '...': '' }}</vue-markdown>
                         </div>
                         <div class="flex items-center">
                             <!--<img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">-->
@@ -37,9 +37,13 @@
 
 <script>
 import RemoteData from '../mixins/RemoteData'
+import VueMarkdown from 'vue-markdown'
 
 export default {
     name: 'Posts',
+    components: {
+     VueMarkdown,
+   },
     mixins: [
         RemoteData({
             postList: 'posts',
@@ -47,7 +51,8 @@ export default {
     ],
     data () {
         return {
-            errorFetch: 'Il y a un problème avec la requète.'
+            errorFetch: 'Il y a un problème avec la requète.',
+            maxCharacters: 15
         }
     }
 }
