24a404eed8e65e66e3bdfa86aa64c1e13aa259b9
Délita Makanda <delita.makanda@gmail.com>
2018-10-06 18:32:11 +0200

    change configrations settinfs

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
