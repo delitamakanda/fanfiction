0070964acbe586b1021e6d1fb07a27c94c460f70
Délita Makanda <delita.makanda@gmail.com>
2018-10-09 22:46:23 +0200

    [fix] reload chapters list

--- src/components/Fanfic.vue
+++ src/components/Fanfic.vue
@@ -20,7 +20,7 @@
                         Catégorie
                       </label>
                       <div class="relative">
-                          <select class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="category" name="category" v-model="fanfic.category">
+                          <select disabled class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="category" name="category" v-model="fanfic.category">
                               <option value="">Sélectionner</option>
                               <option v-for="(option, index) of dataCategories" v-bind:value="option.id">{{ option.name }}</option>
                           </select>
@@ -34,7 +34,7 @@
                           Sous - Catégories
                       </label>
                       <div class="relative">
-                          <select class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="subcategory" name="subcategory" v-model="fanfic.subcategory">
+                          <select disabled class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow" id="subcategory" name="subcategory" v-model="fanfic.subcategory">
                               <option value="">Sélectionner</option>
                               <option v-for="(option, index) of dataSubCategories" v-bind:value="option.id">{{ option.name }}</option>
                           </select>
@@ -158,7 +158,6 @@
 
                 <button @click="deleteStory(fanfic.id)" title="Supprimer l'histoire"><svgicon icon="trash" width="22" height="18" color="#000"></svgicon></button>
             </section>
-
             <section class="update-chapters" v-if="isEditingChapter">
                 <form
                 @submit.prevent="editingChapter"
@@ -171,7 +170,7 @@
                         </label>
                         <Input
                             name="title"
-                            v-model="get_chapter.title"
+                            v-model="chapters.title"
                             placeholder="Titre du chapitre"
                             maxlength="255"
                             required />
@@ -183,7 +182,7 @@
                         <Input
                             type="textarea"
                             name="description"
-                            v-model="get_chapter.description"
+                            v-model="chapters.description"
                             placeholder="Description du chapitre"
                             maxlength="1000"
                             rows="4" />
@@ -192,15 +191,15 @@
                         <label class="block tracking-wide text-grey-darker text-xs font-bold mb-2" for="text">
                           Chapitre
                         </label>
-                        <trumbowyg v-model="get_chapter.text"></trumbowyg>
+                        <trumbowyg v-model="chapters.text"></trumbowyg>
                     </div>
                     <Input
                         type="hidden"
                         name="fanfic"
-                        v-model="get_chapter.fanfic" />
+                        v-model="chapters.fanfic" />
                     <button
                         type="button"
-                        @click="closeChapterEditiong"
+                        @click="closeChapterEditing"
                         class="secondary inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker">
                         Retour à la l'histoire
                     </button>
@@ -227,7 +226,7 @@
                     <router-link class="mt-4 lg:inline-block lg:mt-0 text-teal hover:text-teal-darker" :to="{name: 'NewChapter', params: { fanfic: fanfic.id }}">Ajouter un chapitre</router-link>
                 </div>
             </section>
-            <section class="action">
+            <section class="action" v-if="allComments.length > 0 && comments.length > 0">
                 <h3>Commentaire(s)</h3>
                 <div class="tabs comment-tabs">
                     <ul class="list-reset flex border-b">
@@ -348,9 +347,6 @@ export default {
             fanfic () {
                 return `fanfics/${this.id}`
             },
-            chapters () {
-                return `chapters/${this.id}/list`
-            },
             comments () {
                 return `comments/fanfic/${this.id}/list`
             },
@@ -381,7 +377,6 @@ export default {
         return {
             fanfic: [],
             chapters: [],
-            get_chapter: [],
             comments: [],
             comment: {},
             allComments: [],
@@ -444,7 +439,13 @@ export default {
         this.dataCategories = await this.$fetch('category')
         this.dataSubCategories = await this.$fetch('subcategory')
     },
+    mounted () {
+        this.getChaptersList()
+    },
     methods: {
+        async getChaptersList () {
+            this.chapters = await this.$fetch(`chapters/${this.id}/list`)
+        },
         async deleteChapter (chapterId, index) {
             const message = `Supprimer le chapitre id #${chapterId} ?`
 
@@ -548,16 +549,15 @@ export default {
         },
         editFanfic () {
             this.isEditingFanfic = !this.isEditingFanfic
-            if (this.isEditingChapter) { this.isEditingChapter = false }
         },
         async editChapter (id) {
-            this.get_chapter = await this.$fetch(`chapters/${id}`)
+            this.chapters = await this.$fetch(`chapters/${id}`)
             this.isEditingChapter = true
-            if (this.isEditingFanfic) { this.isEditingFanfic = false }
             this.chapter_id = id
         },
-        closeChapterEditiong () {
+        async closeChapterEditing () {
             this.isEditingChapter = false
+            this.getChaptersList()
         },
         async edit () {
             const result = await this.$fetch(`fanfics/${this.$route.params.id}`, {
@@ -581,14 +581,14 @@ export default {
             const result = await this.$fetch(`chapters/${this.chapter_id}`, {
                 method: 'PUT',
                 body: JSON.stringify({
-                    title: this.get_chapter.title,
-                    description: this.get_chapter.description,
-                    text: this.get_chapter.text,
-                    fanfic: this.get_chapter.fanfic,
+                    title: this.chapters.title,
+                    description: this.chapters.description,
+                    text: this.chapters.text,
+                    fanfic: this.chapters.fanfic,
                     author: this.$state.user.id
                 }),
             })
-            this.isEditingChapter = false
+            this.closeChapterEditing()
         }
     }
 }
