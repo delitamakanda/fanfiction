bca4818aa468954f84449a592880c9d72f28cb79
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-10-15 10:07:05 +0200

    refactor task

--- api/tasks.py
+++ api/tasks.py
@@ -1,4 +1,5 @@
 from celery import task
+from django.template.loader import get_template, render_to_string
 from django.core.mail import send_mail
 from django.core.mail import send_mass_mail
 
@@ -12,8 +13,12 @@ def fanfic_created(fanfic_id):
     Task to send an e-mail notification when an order is successfully created.
     """
     fanfic = Fanfic.objects.get(id=fanfic_id)
-    subject = 'Fanfic id# {}'.format(fanfic.id)
-    message = 'Cher {},\n\nVotre fanfiction {} a été créer avec succès. Son identifiant est le numéro #{}.'.format(fanfic.author.username, fanfic.title, fanfic.id)
-    mail_sent = send_mail(subject, message, "no-reply@fanfiction.com", [fanfic.author.email])
+    subject = 'Fanfiction id# {} - {}'.format(fanfic.id, fanfic.title)
+    template = get_template('mail/fanfic_created_notification.txt')
+    context = {'fanfic': fanfic}
+    msg_text = template.render(context)
+    msg_html = render_to_string('mail/fanfic_created_notification.html', context)
+    # message = 'Cher {},\n\nVotre fanfiction {} a été créer avec succès. Son identifiant est le numéro #{}.'.format(fanfic.author.username, fanfic.title, fanfic.id)
+    mail_sent = send_mail(subject, msg_text, "no-reply@fanfiction.com", [fanfic.author.email], html_message=msg_html)
 
     return mail_sent
