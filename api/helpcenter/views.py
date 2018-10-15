396de99c3f754a188b3f5cc286a05bb816d3591c
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-10-15 14:52:34 +0200

    remove try

--- api/helpcenter/views.py
+++ api/helpcenter/views.py
@@ -55,7 +55,7 @@ def foire_aux_questions_view(request):
   
   
 def communities_view(request):
-	  """
+    """
 	  Communities
 	  """
 	
@@ -64,15 +64,14 @@ def communities_view(request):
 
 
 def fanfic_pdf(request, fanfic_id):
-	  """
-	  Generate pdf output
-	  """
-	
-	  try:
-		  fanfic = Fanfic.objects.filter(status="publié").get(id=fanfic_id)
-		  chapters = Chapter.objects.filter(fanfic=fanfic, status="publié")
-		  html = render_to_string('pdf/fanfic.html', {'fanfic': fanfic, 'chapters': chapters})
-		  response = HttpResponse(content_type='application/pdf')
-		  response['Content-Disposition'] = 'filename="fanfic_{}.pdf"'.format(fanfic.id)
-		  weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + '/styles/base.css')])
-		  return response
+    """
+    Generate pdf output
+    """
+    
+    fanfic = Fanfic.objects.filter(status="publié").get(id=fanfic_id)
+    chapters = Chapter.objects.filter(fanfic=fanfic, status="publié")
+    html = render_to_string('pdf/fanfic.html', {'fanfic': fanfic, 'chapters': chapters})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="fanfic_{}.pdf"'.format(fanfic.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + '/styles/base.css')])
+    return response
