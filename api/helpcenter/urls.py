db7e611ad2ec3c74323d60d046137315e9cdb6f3
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-10-15 14:30:30 +0200

    Update urls.py

--- api/helpcenter/urls.py
+++ api/helpcenter/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('search-ajax-submit', views.SearchAjaxSubmitView.as_view(), name='search-ajax-submit'),
     path('faq', views.foire_aux_questions_view, name='foire_aux_questions'),
     path('forum', views.communities_view, name='communities_view'),
+	path('fanfic/<int:fanfic_id>/pdf', views.fanfic_pdf, name='fanfic_pdf'),
 ]
