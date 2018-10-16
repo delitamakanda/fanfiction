ef574a5970af69f51737438e79ec829a42f01105
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-10-16 16:29:37 +0200

    add view logout

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('password_reset/', auth_views.password_reset, name='password_reset'),
     path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.password_reset_confirm, name='password_reset_confirm'),
     path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
+    path('logout/', auth_views.logout, {'next_page': reverse_lazy('index')}, name='logout'),
 ]
