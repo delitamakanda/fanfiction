b932212d2dc4dcb3368f953b7da0b4a210eb4fc7
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-10-17 10:48:36 +0200

    Update apps.py

--- api/apps.py
+++ api/apps.py
@@ -6,4 +6,4 @@ class ApiConfig(AppConfig):
     verbose_name = "API"
 
     def ready(self):
-        from api.signals import users_like_changed
+        from api.signals import users_like_changed, build_profile_on_user_creation
