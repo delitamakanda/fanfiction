53a77cbe7e1868e61a5abf72d62ce6c035dab01a
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-10-19 12:49:23 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -82,6 +82,7 @@ class FavoritedFanficView(views.APIView):
         serializer = FanficSerializer()
         if serializer.data:
             liked_fanfic(request)
+			create_notification(self.request.user, 'a aimé', serializer.data['title'])
         return Response({'status': 'ok'}, status=status.HTTP_200_OK)
 
 
