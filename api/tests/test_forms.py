06e26f199b82ee4433acec6545f211d8df997f1e
Délita Makanda <delita.makanda@gmail.com>
2018-10-28 22:55:13 +0100

    add tests for pages forum

--- api/tests/test_forms.py
+++ api/tests/test_forms.py
@@ -1,1 +1,58 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
+from api.helpcenter.views import communities_view_new_topic
+from api.models import Board, Topic, Message
+from api.helpcenter.forms import NewTopicForm
 
+class NewTopicFormTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Anime-Mangas', description='bla bla bla.')
+        User.objects.create_user(id=1, username='homer simpson', email='homer.simpson@springface.com', password='test')
+
+    def test_csrf(self):
+        url = reverse('board_topics_new', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+
+    # def test_new_topic_valid_post_data(self):
+    #     url = reverse('board_topics_new', kwargs={'pk': 1})
+    #     data = {
+    #         'subject': 'test test',
+    #         'text': 'text text text message'
+    #     }
+    #     response = self.client.post(url, data)
+    #     self.assertTrue(Topic.objects.exists())
+    #     self.assertTrue(Message.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        url = reverse('board_topics_new', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        url = reverse('board_topics_new', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'text': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Message.objects.exists())
+
+
+    def test_contains_form(self):  # <- new test
+        url = reverse('board_topics_new', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        url = reverse('board_topics_new', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from api.helpcenter.views import communities_view, communities_view_board_topics, communities_view_new_topic
+from api.models import Board
 
-class FanfictionPagesTest(TestCase):
+class HomeTests(TestCase):
     """
     Tests static pages
     """
@@ -8,10 +11,103 @@ class FanfictionPagesTest(TestCase):
         r = self.client.get('/')
         self.assertEqual(r.status_code, 200)
 
+
+class FaqTests(TestCase):
+    """
+    Tests for faq
+    """
     def test_faq_page(self):
         r = self.client.get('/help/faq')
         self.assertEqual(r.status_code, 200)
 
+
+
+class LexiqueTests(TestCase):
+    """
+    Tests for lexique
+    """
     def test_lexique_page(self):
         r = self.client.get('/help/browse/title')
         self.assertEqual(r.status_code, 200)
+
+
+class ForumTests(TestCase):
+    """
+    Tests for forums
+    """
+    def setUp(self):
+        self.board = Board.objects.create(name='Anime-Mangas', description='bla bla bla.')
+        url = reverse('communities_view')
+        self.response = self.client.get(url)
+
+    def test_communities_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_url_resolves_communities_view(self):
+        view = resolve('/help/forum')
+        self.assertEquals(view.func, communities_view)
+
+    def test_communities_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+
+
+class TopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Anime-Mangas', description='bla bla bla.')
+
+    def test_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_topics_url_resolves_board_topics_view(self):
+        view = resolve('/help/forum/1')
+        self.assertEquals(view.func, communities_view_board_topics)
+
+    def test_communities_view_contains_link_to_boards(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        boards_url = reverse('communities_view')
+        self.assertContains(response, 'href="{0}"'.format(boards_url))
+
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        boards_url = reverse('communities_view')
+        new_topic_url = reverse('board_topics_new', kwargs={'pk': 1})
+
+        response = self.client.get(board_topics_url)
+
+        self.assertContains(response, 'href="{0}"'.format(boards_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
+
+class NewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Anime-Mangas', description='bla bla bla.')
+
+    def test_new_topic_view_success_status_code(self):
+        url = reverse('board_topics_new', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_view_not_found_status_code(self):
+        url = reverse('board_topics_new', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_new_topic_url_resolves_board_topics_view(self):
+        view = resolve('/help/forum/1/new')
+        self.assertEquals(view.func, communities_view_new_topic)
+
+    def test_new_topic_view_contains_link_to_boards_topic_view(self):
+        new_topics_url = reverse('board_topics_new', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(new_topics_url)
+        self.assertContains(response, 'href="{0}"'.format(board_topics_url))
