fe79e3aa6def671480976b1f182ec998ab1bc2fc
Délita Makanda <delita.makanda@gmail.com>
2018-11-07 23:26:52 +0100

    change password reset expiration days

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 from decouple import config
 
+from django.urls import reverse
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -199,6 +201,8 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'api.authentication.EmailAuthBackend',
 )
+
+PASSWORD_RESET_TIMEOUT_DAYS = 1
 
 # Asynchronous tasks
 
