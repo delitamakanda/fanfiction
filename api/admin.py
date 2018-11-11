be3ab8a3daa8bdc4a8929f6d1829345c93f86f8d
Délita Makanda <delita.makanda@gmail.com>
2018-11-11 23:36:03 +0100

    import reverse

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
