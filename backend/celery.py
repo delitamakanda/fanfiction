c4693f42e37ac26a0f53f93a041b595a0c3e1773
Délita Makanda <delitamakanda@users.noreply.github.com>
2019-06-26 21:39:15 +0200

    Fix/cron job (#3)
    * Update tasks.py
    
    * Update celery.py
    
    * Update celery.py
    
    * Update celery.py
    
    * Update celery.py
    
    * Update tasks.py
    
    * Update tasks.py
    
    * Update celery.py
    
    * Delete cron_job.txt
    
    * Delete cronjob.config
    
    * fix task

--- backend/celery.py
+++ backend/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
@@ -14,3 +15,18 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('request: {0!r}'.format(self.request))
+
+app.conf.beat_schedule = {
+	'user_email_reminder_every_week': {
+		'task': 'api.tasks.user_email_reminder',
+		'schedule': crontab(hour=7, minute=30, day_of_week=1),
+		'args': ()
+	},
+	'deactivate_inactive_user': {
+		'task': 'api.tasks.deactivate_inactive_user',
+		'schedule': crontab(hour=0, minute=0, day_of_month=1, month_of_year=1),
+		'args': ()
+	}
+}
+
+app.conf.timezone = 'UTC'
