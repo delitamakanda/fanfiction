cfc485d32e9bca2ef5e161df2aeb7500f3c835ab
Délita Makanda <delitamakanda@users.noreply.github.com>
2020-02-25 13:46:05 +0100

    Update scraper.py

--- fanfics/scraper.py
+++ fanfics/scraper.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://www.fanfiction.net/misc/Misc-Comics/"
+r = requests.get(URL)
+
+soup = BeautifulSoup(r.content, 'html5lib')
+
+print(soup.prettify())
+
+
+# https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
