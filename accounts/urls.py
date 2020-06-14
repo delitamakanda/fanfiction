1187c8378a5701278a804edc386dec141070c265
Délita Makanda <delitamakanda@users.noreply.github.com>
2020-06-15 00:24:05 +0200

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('logout/', auth_views.LogoutView.as_view(), {'next_page': reverse_lazy('index')}, name='logout_auth'),
 ]
