a3ba67df0d9cdaa61bbef08646dc1475130e19e2
Délita Makanda <delita.makanda@gmail.com>
2021-03-10 21:16:43 +0100

    refactor endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -27,6 +27,7 @@ from accounts.api.views import (
     AccountProfileDetailView,
     SocialListApiView,
     SocialDestroyApiView,
+    SignupView,
 )
 
 from helpcenter.api.views import (
@@ -72,58 +73,127 @@ from api.views import (
     ApiRootView,
 )
 
+from api.api import (
+    EmailFeedbackView,
+    FavoritedFanficView,
+    UnfavoritedFanficView,
+    FollowUserView,
+    FollowStoriesView,
+    DeleteAccountView,
+    ContactMailView,
+    FlatPagesView,
+    FlatPagesByTypeView,
+    FlatPagesHTMLByTypeView,
+    ContentTypeView,
+    NotificationListView,
+)
+
+from api.api_auth import (
+    CheckoutUserView,
+    UserCreateView,
+    LoginView,
+    LogoutView,
+    SocialSignUp,
+    ChangePasswordView,
+    RemovePhotoFromAccount,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('help/', include(('helpcenter.urls', 'helpcenter'), namespace='helpcenter')),
-    path('', TemplateView.as_view(template_name='frontend/index.html'), name='index'),
+    path('', TemplateView.as_view(
+        template_name='frontend/index.html'), name='index'),
 ]
 
-urlpatterns += [    
+urlpatterns += [
     path('api/', ApiRootView.as_view(), name=ApiRootView.name),
     path('api/docs/', include_docs_urls(title='Fanfiction API', public=False)),
 
-    path('api/users/<str:user__username>/account', UserFanficDetailView.as_view(), name='user-list'),
-    path('api/users/<int:pk>', UserDetailView.as_view(), name='user-detail'),
-    path('api/users/<str:user__username>/profile', AccountProfileDetailView.as_view(), name='user-edit-account-profile'),
-    path('api/users/<int:account>/socialaccount', SocialListApiView.as_view(), name='socialaccount-view'),
-    path('api/users/social-account', SocialListApiView.as_view(), name='socialaccount-createview'),
-    path('api/users/social-account/<int:pk>/delete', SocialDestroyApiView.as_view(), name='socialaccount-destroy'),
+    path('api/user/', CheckoutUserView.as_view(), name='user'),
+    path('api/signup/', UserCreateView.as_view(), name='signup'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/social_sign_up/', SocialSignUp.as_view(), name="social_sign_up"),
+    path('api/change-password/', ChangePasswordView.as_view(),
+         name='change-password'),
+    path('api/remove-photo/<int:pk>/',
+         RemovePhotoFromAccount.as_view(), name='remove-photo'),
+
+    path('api/feedback/', EmailFeedbackView.as_view(), name='feedback'),
+    path('api/favorite/', FavoritedFanficView.as_view(), name='favorite'),
+    path('api/unfavorite/', UnfavoritedFanficView.as_view(), name='unfavorite'),
+    path('api/follow-stories/', FollowStoriesView.as_view(), name='follow-stories'),
+    path('api/follow-user/', FollowUserView.as_view(), name='follow-user'),
+    path('api/disable-account/', DeleteAccountView.as_view(), name='disable-account'),
+    path('api/contact-mail/', ContactMailView.as_view(), name='contact-mail'),
+    path('api/pages/<str:type>/',
+         FlatPagesByTypeView.as_view(), name='pages-by-type'),
+    path('api/page/<str:type>/html/',
+         FlatPagesHTMLByTypeView.as_view(), name='pages-html-by-type'),
+    path('api/pages/', FlatPagesView.as_view(), name='all-pages'),
+    path('api/notifications/', NotificationListView.as_view(), name='notifications'),
+    path('api/contenttype/<int:pk>/',
+         ContentTypeView.as_view(), name='contenttype-detail'),
+
+    path('api/users/<str:user__username>/account/',
+         UserFanficDetailView.as_view(), name='user-list'),
+    path('api/users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('api/users/<str:user__username>/profile/',
+         AccountProfileDetailView.as_view(), name='user-edit-account-profile'),
+    path('api/users/<int:account>/socialaccount/',
+         SocialListApiView.as_view(), name='socialaccount-view'),
+    path('api/users/social-account', SocialListApiView.as_view(),
+         name='socialaccount-createview'),
+    path('api/users/social-account/<int:pk>/delete/',
+         SocialDestroyApiView.as_view(), name='socialaccount-destroy'),
+     path('api/sign_up/', SignupView.as_view(), name="sign_up"),
 
     path('api/faq/', FoireAuxQuestionsApiView.as_view(), name='faq'),
     path('api/lexique/', LexiqueApiView.as_view(), name='lexique'),
 
     path('api/category/', CategoryListView.as_view(), name='category-list'),
-    path('api/category/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('api/category/<int:pk>/',
+         CategoryDetailView.as_view(), name='category-detail'),
     path('api/subcategory/', SubCategoryListView.as_view(), name='subcategory-list'),
-    path('api/subcategory/<int:pk>/', SubCategoryDetailView.as_view(), name='subcategory-detail'),
+    path('api/subcategory/<int:pk>/',
+         SubCategoryDetailView.as_view(), name='subcategory-detail'),
 
     path('api/tags/', TagListAPIView.as_view(), name='tag-list'),
     path('api/posts/', PostListAPIView.as_view(), name='post-list'),
     path('api/posts/create/', PostCreateAPIView.as_view(), name='post-create'),
-    path('api/posts/<int:pk>/update/', PostUpdateAPIView.as_view(), name='post-update'),
-    path('api/posts/<int:pk>/remove/', PostUpdateAPIView.as_view(), name='post-delete'),
+    path('api/posts/<int:pk>/update/',
+         PostUpdateAPIView.as_view(), name='post-update'),
+    path('api/posts/<int:pk>/remove/',
+         PostUpdateAPIView.as_view(), name='post-delete'),
     path('api/posts/<str:slug>/', PostDetailAPIView.as_view(), name='post-detail'),
 
     path('api/genres/', GenresListView.as_view(), name='genre-list'),
     path('api/classement/', ClassementListView.as_view(), name='classement-list'),
     path('api/status/', StatusListView.as_view(), name='status-list'),
     path('api/fanfics/', FanficCreateApiView.as_view(), name='fanfic-list'),
-    path('api/fanfics/<str:username>/', FanficCreateApiView.as_view(), name='fanfic-list-by-author'),
-    path('api/fanfics/<str:slug>/detail/', FanficDetailView.as_view(), name='fanfic-detail'),
-    path('api/fanfics/<int:pk>/fanfic-detail/', FanficUpdateDetailView.as_view(), name='fanfic-update-detail'),
+    path('api/fanfics/<str:username>/',
+         FanficCreateApiView.as_view(), name='fanfic-list-by-author'),
+    path('api/fanfics/<str:slug>/detail/',
+         FanficDetailView.as_view(), name='fanfic-detail'),
+    path('api/fanfics/<int:pk>/fanfic-detail/',
+         FanficUpdateDetailView.as_view(), name='fanfic-update-detail'),
 
     path('api/comments/', CommentCreateApiView.as_view(), name='comment-create'),
     path('api/comments-list/', CommentListApiView.as_view(), name='comment-list'),
 
-    path('api/chapters/<int:fanfic>/list/', ChapterCreateApiView.as_view(), name='chapter-list-by-fanfic'),
+    path('api/chapters/<int:fanfic>/list/',
+         ChapterCreateApiView.as_view(), name='chapter-list-by-fanfic'),
     path('api/chapters/create/', ChapterCreateApiView.as_view(), name='chapter-list'),
-    path('api/chapters/<int:pk>/', ChapterDetailView.as_view(), name='chapter-detail'),
+    path('api/chapters/<int:pk>/',
+         ChapterDetailView.as_view(), name='chapter-detail'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
     path('markdownx/', include('markdownx.urls')),
