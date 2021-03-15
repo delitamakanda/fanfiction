d71a04f03d854d4f3aea017a4e44bba5c691f525
Délita Makanda <delita.makanda@gmail.com>
2021-03-15 21:42:19 +0100

    fix settings double import

--- backend/settings.py
+++ backend/settings.py
@@ -75,7 +75,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-	'oauth2_provider.middleware.OAuth2TokenMiddleware',
+    'oauth2_provider.middleware.OAuth2TokenMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
