f52f7ea541e9ca3e1af802154d9048f3d37dc872
Délita Makanda <delitamakanda@users.noreply.github.com>
2021-10-04 23:03:27 +0200

    Update views.py

--- fanfics/api/views.py
+++ fanfics/api/views.py
@@ -123,11 +123,11 @@ class FanficDetailView(generics.RetrieveAPIView):
                 self.request.session[session_key] = True
 
             r = recommender.Recommender()
-            most_liked_fanfics = Fanfic.objects.filter(status='publié').order_by('-total_likes')[:10]
+			most_liked_fanfics = Fanfic.objects.filter(status='publié').order_by('-total_likes')[:10]
 			print(most_liked_fanfics)
 			for most_liked_fanfic in most_liked_fanfics:
 				liked_fanfics = r.fanfics_liked([instance, most_liked_fanfic])
-				
+
 			serializer = self.get_serializer(instance)
             data = serializer.data
             return Response(data, status=status.HTTP_200_OK)
