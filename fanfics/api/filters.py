8eda2c2f4dffc0e66d5caf6c2a5bb23412d5ac46
Délita Makanda <delitamakanda@users.noreply.github.com>
2021-10-17 23:39:52 +0200

    Update filters.py

--- fanfics/api/filters.py
+++ fanfics/api/filters.py
@@ -5,23 +5,17 @@ from django_filters.fields import CSVWidget
 
 from fanfics.models import Fanfic
 
-class MultipleField(MultipleChoiceField):
-    def valid_value(self, value):   
-        return True
 
-class MultipleFilter(df_filters.MultipleChoiceFilter):
-    field_class = MultipleField
+class FanficFilter(django_filters.FilterSet):
+    genres = django_filters.MultipleChoiceFilter(
+        choices=Fanfic.GENRES_CHOICES, action=lambda queryset, value:
+        queryset.filter(genres__genres__in=value))
 
-class FanficFilter(df_filters.FilterSet):
-    #genres = MultipleFilter(lookup_expr="icontains", field_name = "genres", widget=CSVWidget)
-	genres = df_filters.MultipleChoiceFilter(
-		choices=Fanfic.GENRES_CHOICES,
-		action=lambda queryset, value:
-			queryset.filter(genres__genres__in=value)
-	)
     title = django_filters.CharFilter(field_name='title', lookup_expr='iexact')
-    description = django_filters.CharFilter(field_name='description', lookup_expr='iexact')
-    synopsis = django_filters.CharFilter(field_name='synopsis', lookup_expr='iexact')
+    description = django_filters.CharFilter(
+        field_name='description', lookup_expr='iexact')
+    synopsis = django_filters.CharFilter(
+        field_name='synopsis', lookup_expr='iexact')
 
     class Meta:
         model = Fanfic
