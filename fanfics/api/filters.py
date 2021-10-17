f94b260152e2c5886cc5f7685441243a8c4db074
Délita Makanda <delitamakanda@users.noreply.github.com>
2021-10-17 23:18:32 +0200

    Update filters.py

--- fanfics/api/filters.py
+++ fanfics/api/filters.py
@@ -12,7 +12,12 @@ class MultipleFilter(df_filters.MultipleChoiceFilter):
     field_class = MultipleField
 
 class FanficFilter(df_filters.FilterSet):
-    genres = MultipleFilter(lookup_expr="icontains", field_name = "genres", widget=CSVWidget)
+    #genres = MultipleFilter(lookup_expr="icontains", field_name = "genres", widget=CSVWidget)
+	genres = df_filters.MultipleChoiceFilter(
+    choices=Fanfic.GENRES_CHOICES,
+    action=lambda queryset, value:
+        queryset.filter(genres__genres__in=value)
+)
     title = django_filters.CharFilter(field_name='title', lookup_expr='iexact')
     description = django_filters.CharFilter(field_name='description', lookup_expr='iexact')
     synopsis = django_filters.CharFilter(field_name='synopsis', lookup_expr='iexact')
