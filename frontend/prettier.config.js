c5cf1a9e8a1794019e8179af415f7ba0af42eb7f
Délita Makanda <delita.makanda@gmail.com>
2022-08-14 01:53:04 +0200

    new tempalte

---
+++ frontend/prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    endOfLine: "lf",
+    jsxSingleQuote: true,
+    printWidth: 80,
+    proseWrap: "never",
+    quoteProps: "as-needed",
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: "es5",
+    useTabs: false,
+    vueIndentScriptAndStyle: false,
+};
---
+++ frontend/src/plugins/vue-select.ts
@@ -0,0 +1,4 @@
+import 'vue-select/dist/vue-select.css';
+import vSelect from 'vue-select';
+
+export const vueSelect = vSelect;
