bfaaa92b01ac1ba5ad7f5eab75dd3c0ad251a30d
Délita Makanda <delita.makanda@gmail.com>
2022-08-13 22:08:05 +0200

    upgrade vue components

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        vue 3
+        <button @click="increase">Clicked {{ count }} times.</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const count = ref(0);
+        const increase = () => {
+            count.value++;
+        }
+
+        return {
+            count, increase
+        }
+    }
+})
+</script>
---
+++ frontend/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = (env = {}) => {
+    return {
+        mode: env.prod ? 'production' : 'development',
+        devtool: env.prod ? 'source-map' : 'eval-cheap-module-source-map',
+        entry: path.resolve(__dirname, './src/main.ts'),
+        output: {
+            path: path.resolve(__dirname, './dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/],
+                    }
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.ts', '.vue', '.json'],
+            alias: {
+                'vue': '@vue/runtime-dom'
+            }
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new BundleTracker({
+                filename: './webpack-stats.json',
+                publicPath: 'http://0.0.0.0:8080/'
+            }),
+        ],
+        devServer: {
+            headers: {
+                'Access-Control-Allow-Origin': '\*',
+            },
+            host: '0.0.0.0',
+            port: '8080',
+            hot: true,
+            static: __dirname,
+            client: {
+                overlay: true,
+                logging: 'info',
+                progress: true,
+            },
+        }
+    }
+}
