d3c2fbe2cbc19c5d494ee1778fd067e953c5c782
Délita Makanda <delita.makanda@gmail.com>
2022-08-17 22:01:08 +0200

    refactor

---
+++ frontend/src/components/common/Tag.vue
@@ -0,0 +1,27 @@
+<template>
+    <component :is="component" class="tag" v-bind="$attrs">
+        <slot /> 
+    </component>
+</template>
+
+<script lang="ts">
+export default {
+    inheritAttrs: false, 
+    props: {
+        component: { 
+            type: String, 
+            default: 'div',
+        },
+    },
+}
+</script>
+
+<style lang="scss"> 
+.tag {
+    padding: 3px 2px;
+    font-weight: bold;
+    color: white;
+    background-color: purple;
+    border-radius: 8px;
+} 
+</style>
