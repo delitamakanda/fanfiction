ba4c7c4bbc0ab9acd9f6d38faf7fafb137d49bbf
Délita Makanda <delita.makanda@gmail.com>
2022-08-31 03:33:38 +0200

    refactor post

--- frontend/src/views/info/components/PostCard.vue
+++ frontend/src/views/info/components/PostCard.vue
@@ -7,7 +7,7 @@
             </span>
             <span class="text-sm">{{ formatDateTime(post.created) }}</span>
         </div>
-        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{{ post.title }}</a></h2>
+        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><router-link :to="`/announcement/view/${post.slug}`">{{ post.title }}</router-link></h2>
         <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
             <markdown :source="truncateText(post.content)" :html="true" :linkify="true"></markdown>
         </p>
