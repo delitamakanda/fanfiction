d6096660d21c3b71638478c054010db7db62c3d9
Délita Makanda <delita.makanda@gmail.com>
2024-12-07 22:01:23 +0100

    change model

--- fanfics/models.py
+++ fanfics/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from multiselectfield import MultiSelectField
 
 from django.db import models
@@ -9,91 +10,96 @@ from categories.models import Category, SubCategory
 
 
 class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='publié')
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='publié')
 
 
 class Fanfic(models.Model):
-    GENRES_CHOICES = (
-        ('RO', 'Romance'),
-        ('SU', 'Surnaturel'),
-        ('ER', 'Erotique'),
-        ('DR', 'Drame'),
-        ('AM', 'Amitié'),
-        ('AC', 'Action-Aventure'),
-        ('SC', 'School-Fic'),
-        ('MY', 'Mystère'),
-        ('GE', 'Général'),
-        ('DCED', 'De cape et d\'épée'),
-        ('LE', 'Lemon'),
-        ('HU', 'Humour'),
-        ('OS', 'One-Shot'),
-        ('SUS', 'Suspense'),
-        ('TH', 'Thriller'),
-        ('HO', 'Horreur'),
-        ('HF', 'Heroic Fantasy'),
-        ('TR', 'Tragédie'),
-        ('CO', 'Cross-Over'),
-    )
-    STATUS_CHOICES = (
-        ('brouillon', 'Brouillon'),
-        ('publié', 'Publié'),
-    )
-    CLASSEMENT_CHOICES = (
-        ('g', 'G'),
-        ('13', '13+'),
-        ('r', 'R'),
-        ('18', '18+'),
-    )
-    author = models.ForeignKey(
-        User, related_name="fanfics", on_delete=models.CASCADE)
-    title = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, db_index=True,
-                            unique=True, blank=True, null=True)
-    synopsis = models.CharField(max_length=1000, blank=True, default='')
-    credits = models.CharField(max_length=255, blank=True, default='')
-    picture = models.CharField(max_length=1000, blank=True, null=True)
-    language = models.CharField(max_length=2, default='fr')
-    description = models.CharField(max_length=1000, blank=True, default='')
-    classement = models.CharField(
-        max_length=2, choices=CLASSEMENT_CHOICES, default='g')
-    genres = MultiSelectField(choices=GENRES_CHOICES, max_length=4)
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(blank=True, null=True)
-    was_featured_in_home = models.BooleanField(default=False)
-    status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default='brouillon')
-    users_like = models.ManyToManyField(
-        User, related_name='fanfics_liked', blank=True)
-    total_likes = models.PositiveIntegerField(db_index=True, default=0)
-    objects = models.Manager()
-    published = PublishedManager()
-    category = models.ForeignKey(
-        Category, related_name="categories", on_delete=models.CASCADE)
-    subcategory = models.ForeignKey(
-        SubCategory, related_name="sub_categories", on_delete=models.CASCADE)
-    completed = models.BooleanField(default=False)
-    views = models.PositiveIntegerField(default=0)
-    fanfic_is_scraped = models.BooleanField(default=False)
-    link_fanfic = models.URLField(max_length=255, blank=True, null=True)
+	GENRES_CHOICES = (
+		('RO', 'Romance'),
+		('SU', 'Surnaturel'),
+		('ER', 'Erotique'),
+		('DR', 'Drame'),
+		('AM', 'Amitié'),
+		('AC', 'Action-Aventure'),
+		('SC', 'School-Fic'),
+		('MY', 'Mystère'),
+		('GE', 'Général'),
+		('DCED', 'De cape et d\'épée'),
+		('LE', 'Lemon'),
+		('HU', 'Humour'),
+		('OS', 'One-Shot'),
+		('SUS', 'Suspense'),
+		('TH', 'Thriller'),
+		('HO', 'Horreur'),
+		('HF', 'Heroic Fantasy'),
+		('TR', 'Tragédie'),
+		('CO', 'Cross-Over'),
+	)
+	STATUS_CHOICES = (
+		('brouillon', 'Brouillon'),
+		('publié', 'Publié'),
+	)
+	CLASSEMENT_CHOICES = (
+		('g', 'G'),
+		('13', '13+'),
+		('r', 'R'),
+		('18', '18+'),
+	)
+	author = models.ForeignKey(
+		User, related_name="fanfics", on_delete=models.CASCADE)
+	title = models.CharField(max_length=255, db_index=True)
+	slug = models.SlugField(max_length=255, db_index=True,
+							unique=True, blank=True, null=True)
+	synopsis = models.CharField(max_length=1000, blank=True, default='')
+	credits = models.CharField(max_length=255, blank=True, default='')
+	picture = models.CharField(max_length=1000, blank=True, null=True)
+	language = models.CharField(max_length=2, default='fr')
+	description = models.CharField(max_length=1000, blank=True, default='')
+	classement = models.CharField(
+		max_length=2, choices=CLASSEMENT_CHOICES, default='g')
+	genres = MultiSelectField(choices=GENRES_CHOICES, max_length=4)
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(blank=True, null=True)
+	was_featured_in_home = models.BooleanField(default=False)
+	status = models.CharField(
+		max_length=10, choices=STATUS_CHOICES, default='brouillon')
+	users_like = models.ManyToManyField(
+		User, related_name='fanfics_liked', blank=True)
+	total_likes = models.PositiveIntegerField(db_index=True, default=0)
+	objects = models.Manager()
+	published = PublishedManager()
+	category = models.ForeignKey(
+		Category, related_name="categories", on_delete=models.CASCADE)
+	subcategory = models.ForeignKey(
+		SubCategory, related_name="sub_categories", on_delete=models.CASCADE)
+	completed = models.BooleanField(default=False)
+	views = models.PositiveIntegerField(default=0)
+	fanfic_is_scraped = models.BooleanField(default=False)
+	link_fanfic = models.URLField(max_length=255, blank=True, null=True)
 
-    class Meta:
-        ordering = ('-publish',)
-        verbose_name = 'fanfic'
-        verbose_name_plural = 'fanfics'
+	class Meta:
+		ordering = ('-publish',)
+		verbose_name = 'fanfic'
+		verbose_name_plural = 'fanfics'
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Fanfic, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.title)
+		super(Fanfic, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
-    def most_liked_fanfics(self):
-        return Fanfic.objects.filter(status='publié').order_by('-total_likes')[:10]
+	@staticmethod
+	def most_liked_fanfics():
+		return Fanfic.objects.filter(status='publié').order_by('-total_likes')[:10]
 
-    def newest_fanfics(self):
-        return Fanfic.objects.filter(status='publié').order_by('-publish')[:10]
+	@staticmethod
+	def newest_fanfics(self):
+		return Fanfic.objects.filter(status='publié').order_by('-publish')[:10]
+
+	def get_absolute_url(self):
+		return reverse('fanfic_detail', args=[str(self.id), self.slug])
 
