e240555fc14b45dfacb4be19edfa4cd3af116945
Délita Makanda <delita.makanda@gmail.com>
2024-12-25 23:56:16 +0100

    fix views

--- fanfics/api/views.py
+++ fanfics/api/views.py
@@ -111,6 +111,17 @@ class FanficViewSet(TemplateViewSet):
 				status='publié').order_by('-total_likes')[:10]
 		for most_liked_fanfic in most_liked_fanfics:
 			r.fanfics_liked([instance, most_liked_fanfic])
+		serializer = self.get_serializer(instance)
+		return Response(serializer.data)
+
+	def list(self, request, *args, **kwargs):
+		queryset = self.filter_queryset(self.get_queryset())
+		page = self.paginate_queryset(queryset)
+		if page is not None:
+			serializer = self.get_serializer(page, many=True)
+			return self.get_paginated_response(serializer.data)
+		serializer = self.get_serializer(queryset, many=True)
+		return Response(serializer.data)
 
 
 class RecommendedFanficViewSet(TemplateViewSet):
