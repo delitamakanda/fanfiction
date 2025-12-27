d65a81eca71303fceb3f0acf360b78e0bd7e40e5
Délita Makanda <delita.makanda@gmail.com>
2025-12-27 21:28:54 +0100

    refactor comment

--- comments/models.py
+++ comments/models.py
@@ -1,25 +1,22 @@
 from django.db import models
-from django.urls import reverse
-
+from django.contrib.auth.models import User
 from fanfics.models import Fanfic
 from chapters.models import Chapter
 
 class Comment(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	fanfic = models.ForeignKey(Fanfic, related_name='comments', on_delete=models.CASCADE)
-	name = models.CharField(max_length=80, db_index=True)
-	email = models.EmailField(blank=True, null=True)
+	chapter = models.ForeignKey(Chapter, related_name='comments', on_delete=models.CASCADE, blank=True, null=True)
 	body = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True, blank=True, null=True)
 	active = models.BooleanField(default=True)
-	in_reply_to = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
-	chapter = models.ForeignKey(Chapter, related_name='comments_by_chapters', on_delete=models.CASCADE, blank=True, null=True)
+	in_reply_to = models.ForeignKey('self', null=True, on_delete=models.CASCADE, related_name='replies'  )
 
 	class Meta:
-		ordering = ('created',)
+		ordering = ('-created',)
 		verbose_name = 'comment'
 		verbose_name_plural = 'comments'
 
 	def __str__(self):
-		return 'Comment by {} on {}'.format(self.name, self.fanfic)
-	def get_absolute_url(self):
-		return reverse('comments:detail', kwargs={'pk': self.pk})
+		return 'Comment by {} on {}'.format(self.author.username, self.fanfic.title)
--- comments/urls.py
+++ comments/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path
 from comments.api.views import (
-    CommentCreateApiView,
-    CommentListApiView,
-CommentUpdateApiView,
+CommentViewSet,
 )
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'', CommentViewSet, basename='comments')
 
 app_name = 'comments'
 
-urlpatterns = [
-	path('create/', CommentCreateApiView.as_view(), name='comment-create'),
-	path('<int:pk>/update/', CommentUpdateApiView.as_view(), name='comment-update'),
-	path('', CommentListApiView.as_view(), name='comment-list'),
-]
+urlpatterns = router.urls
