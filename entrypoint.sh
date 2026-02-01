60c888315a44b768c7b921f8ef8f2f877677dae4
Délita Makanda <delita.makanda@gmail.com>
2026-02-02 00:36:58 +0100

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -euo pipefail
+
+export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-backend.settings_production}"
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 exec "$@"
