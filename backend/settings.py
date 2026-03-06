2fbd38f4fa21080770bb792da5b5f56e83448953
Délita Makanda <delita.makanda@gmail.com>
2026-03-06 23:32:12 +0100

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -134,10 +134,6 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-		'CONN_MAX_AGE': 3600,
-		'OPTIONS': {
-            'connect_timeout': 15,
-        },
 	}
 }
 
