49ba4f830bc221be870302c3b61603ff170e74eb
Délita Makanda <delitamakanda@users.noreply.github.com>
2026-05-08 23:34:55 +0200

    Fix syntax error in DEBUG condition

--- backend/urls.py
+++ backend/urls.py
@@ -46,7 +46,7 @@ urlpatterns += [
 	path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 
-if settings.DEBUGand not settings.TESTING:
+if settings.DEBUG and not settings.TESTING:
 	urlpatterns += static(settings.MEDIA_URL,
 						  document_root=settings.MEDIA_ROOT)
 	urlpatterns += static(settings.STATIC_URL,
