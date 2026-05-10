43cc5f2caaa633ac022b364eb5b7efabde0a8829
Délita Makanda <delita.makanda@gmail.com>
2026-05-10 19:55:21 +0200

    add ai features

--- chapters/urls.py
+++ chapters/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from chapters.api.views import (
      ChapterListApiView,
      ChapterCreateApiView,
-     ChapterDetailView
+     ChapterDetailView,
+generate_chapter_summary,
+list_styles_prompts,
+autosave_chapter
 )
 
 app_name = 'chapters'
@@ -13,4 +16,8 @@ path('<int:fanfic>/list/',
     path('create/', ChapterCreateApiView.as_view(), name='chapter-create'),
     path('<int:pk>/',
          ChapterDetailView.as_view(), name='chapter-detail'),
+	path('generate', generate_chapter_summary, name='generate'),
+	path('styles-prompts', list_styles_prompts, name='styles-prompts'),
+	# fanfic_id is passed as a parameter to the view
+	path('<int:fanfic_id>/autosave', autosave_chapter, name='autosave'),
 ]
