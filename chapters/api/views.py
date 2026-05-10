121c80936f1f933914c665a370f7320a4ec1afeb
Délita Makanda <delita.makanda@gmail.com>
2026-05-10 19:55:21 +0200

    add ai features

--- chapters/api/views.py
+++ chapters/api/views.py
@@ -1,6 +1,10 @@
+from Crypto.SelfTest.Hash.test_keccak import data
 from rest_framework import generics, permissions
 from rest_framework.exceptions import NotFound
 from rest_framework.throttling import UserRateThrottle
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from chapters.api.serializers import ChapterSerializer, ChapterFormattedSerializer
 
@@ -9,6 +13,7 @@ from chapters.models import Chapter
 from api import custompagination
 
 import logging
+import openai
 
 logger = logging.getLogger(__name__)
 
@@ -63,3 +68,73 @@ class ChapterDetailView(ChapterBaseAPIView, generics.RetrieveUpdateDestroyAPIVie
         instance = serializer.save(author=self.request.user)
         instance.full_clean()
         self.perform_create_or_update(serializer)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_styles_prompts(request):
+    styles = ['neutre', 'romantique', 'dramatique', 'comique','mystérieux']
+    return Response(styles)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def generate_chapter_summary(request):
+    prompt = request.data.get('prompt', '')
+    style = request.data.get('style', 'neutre')
+
+    styles = {
+        'neutre': 'avec un ton neutre et équilibré',
+        'romantique': 'avec un ton romantique et poétique',
+        'dramatique': 'avec une intensité dramatique',
+        'comique': 'avec un ton humoristique',
+        'mystérieux': 'avec une ambiance inquétante',
+    }
+
+    prompt_style = styles.get(style, 'avec un ton neutre et équilibré')
+    system_message = f"Tu es un écrivain de fanfiction expert. Ecris {prompt_style}""'"
+
+    response = openai.Completion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=600,
+    )
+
+    return Response({
+        'generated_summary': response["choices"][0]["message"]["content"],
+    })
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def autosave_chapter(request, fanfic_id):
+    data_rq = request.data
+    chapter, created = Chapter.objects.update_or_create(
+        author=request.user,
+        fanfic=fanfic_id,
+        title=data_rq['title'],
+        description=data_rq['description'],
+        text=data_rq['text'],
+        order=data_rq['order'],
+        status=data_rq['status'],
+    )
+    return Response({
+        'message': 'Autosaved',
+        'id': chapter.id,
+        'created': created,
+        'updated': chapter.updated,
+        'published': chapter.published,
+        'is_published': chapter.is_published,
+        'fanfic': chapter.fanfic.title,
+        'title': chapter.title,
+        'description': chapter.description,
+        'text': chapter.text,
+        'order': chapter.order,
+        'status': chapter.status,
+        'author': chapter.author.username,
+        'fanfic_id': chapter.fanfic.id,
+        'author_id': chapter.author.id,
+    })
